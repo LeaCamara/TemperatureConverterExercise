8dad506ed2372f8cbc670b318dc6c1fa3a72cce1
LeaCamara <lealindacamara@gmail.com>
2017-01-22 21:09:52 -0600

    fixed a bug & completed exercise

--- converter.js
+++ converter.js
@@ -25,7 +25,6 @@ function pressed(e) {
 }
 // checking which key was pressed, and if it was 13, or the enter key, then execute the determineConverter function, and pass along the event
 
-
 // *****  ANOTHER WAY TO DO THE ENTER KEY EVENT: **********************
 
 // document.getElementById("input-field").onkeypress = function(e) {
@@ -36,6 +35,19 @@ function pressed(e) {
 // onkeypress is a method of the element you're getting
 // P.S. This alerts when you press Enter ANYWHERE on the page b/c "document":
 // document.onkeypress = function(e) { if(e.key === "Enter"){ alert('you pressed Enter!')}}
+
+// This function fires once the keypress event is detected & determines which conversion should happen based on which radio button is selected.
+function determineConversion(clickEvent) {
+  var userTemp = document.getElementById("input-field").value;
+// Placing inside function b/c of scope. Console.log tests show that we definitely need this here.
+// .value returns what is in the input field; gets whatever value the user entered into the input field
+  if (document.getElementById("Celsius-button").checked) {
+  toCelsius(userTemp);
+  } else if (document.getElementById("Fahrenheit-button").checked) {
+  toFahrenheit(userTemp);
+  }
+}
+// no need to have else, alert to select a button b/c we set one of the buttons to be selected by default
 
 function toCelsius(userTemp) {
   console.log("userTemp in C function", userTemp);
@@ -50,7 +62,6 @@ function toCelsius(userTemp) {
   output.innerHTML = `<p style="color:green">${finalTemp}<p>`;
   // output.innerHTML = (finalTemp); -- would work but doesn't look as good
   }
-
 }
 
 // 7. If the temperature is greater than 90F/32C the color of the converted temperature should be red.
@@ -71,39 +82,19 @@ function toFahrenheit (userTemp) {
   } else {
   output.innerHTML = `<p style="color:green">${finalTemp}<p>`;
   }
-
 }
-
 // Celsius to Fahrenheit:   (°C × 9/5) + 32 = °F
-
 // Math.ceil gives a whole number rather than a huge long decimal
 
 
-
-
-
-// This function fires once the keypress event is detected & determines which conversion should happen based on which radio button is selected.
-function determineConversion(clickEvent) {
-  var userTemp = document.getElementById("input-field").value;
-// Placing inside function b/c of scope. Console.log tests show that we definitely need this here.
-// .value returns what is in the input field; gets whatever value the user entered into the input field
-  if (document.getElementById("Celsius-button").checked) {
-  toCelsius(userTemp);
-  } else if (document.getElementById("Fahrenheit-button").checked) {
-  toFahrenheit(userTemp);
-  }
-}
-
-// no need to have else, alert to select a button b/c we set one of the buttons to be selected by default
-
 // 5. clear-button: when clicked, clears any text in the input field.
 var clearButton = document.getElementById("clear-button").addEventListener("click", function(event) {
-  "input-field".value = "";
-  "converted-temp-output".innerHTML = "";
+  document.getElementById("input-field").value = "";
+  document.getElementById("converted-temp-output").innerHTML = "";
   document.getElementById("Celsius-button").checked = false;
   document.getElementById("Fahrenheit-button").checked = false;
 });
 
 // you can chain everything together, or you can break event listeners up and call a function. Either way works!
 //inside the clearButton function that fires after the click event, the input & output values are set to an empty string
-//the radio buttons have an attribute on them called "checked" and when it's checked, it's true, and when it's not, it's false. Both are set to false so they'll be reset when the button is clicked.
+//the radio buttons have an attribute on them called "checked". Both are set to false so they'll be reset when the button is clicked.
